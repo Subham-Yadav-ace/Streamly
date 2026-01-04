131ba98ef4ac8824582b6cf143a8f5b39d9085be
Subham-Yadav-ace <aceknightkj@gmail.com>
2026-01-04 16:08:45 +0530

    mongoDb connected

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+dotenv.config({
+  path: "./.env",
+});
+import connectDB from "./db/index.js";
+connectDB();
+/*
+import express from "express";
+const app=express();
+
+;(async()=>{
+    try{
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       app.on("error",(error)=>{
+           console.log("error");
+           throw error
+       })
+       app.listen(process.env.PORT,()=>console.log(`server is running on port ${process.env.PORT}`))
+    }catch(error){
+        console.log(error);
+        throw err
+    }
+})()*/
